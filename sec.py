4defa74fc6a3104644454a3a1a8b2801543b5fc0
Pedro Costa <antonhyonhi@gmail.com>
2024-09-09 16:15:46 -0300

    arquivo crashou de novo
    Renamed "exploration_recovery" back to
    "exploration_6"
    
    Forgot to add the deletion
    
    Found out the earnings are not adjusted.
    
    Added more columns to analisys
    
    Best results so far
    
    Removed expensive histogram
    
    Even better results!
    
    TUrns out my analisy only helps a little for certain companies.
    
    First setps towards final report
    
    Antes de deletar teste ruim
    
    Few things to end report
    
    Final report ready
    
    Renamed final report
    
    Extra stuff to prove viability
    
    Started correlation and volatility analisys.

--- sec.py
+++ sec.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import json
+from tqdm.notebook import tqdm
 from datetime import timedelta
 
-def load_company(cik: int) -> dict | None:
+def load_company(cik: int, directory) -> dict | None:
     try: 
-        return json.load(open(f'data/sec/companyfacts/CIK{str(cik).zfill(10)}.json'))
+        return json.load(open(f'{directory}/CIK{str(cik).zfill(10)}.json'))
     except FileNotFoundError:
         return None
 
@@ -14,9 +15,10 @@ def get_time_series(sec_data: dict, col_name: str, currency: str='USD') -> pd.Da
     return time_series
 
 def convert_datetimes(time_series: pd.DataFrame):
-    time_series['start'] = pd.to_datetime(time_series['start'])
-    time_series['end'] = pd.to_datetime(time_series['end'])
     time_series['filed'] = pd.to_datetime(time_series['filed'])
+    time_series['end'] = pd.to_datetime(time_series['end'])
+    try: time_series['start'] = pd.to_datetime(time_series['start']) 
+    except KeyError: time_series['start'] = time_series['end'] - timedelta(days=90)
 
 def add_time_offsets(time_series: pd.DataFrame):
     time_series['delay'] = time_series['filed'] - time_series['end']
@@ -59,29 +61,31 @@ def add_q4(fy_quarters: pd.DataFrame, fiscal_year: pd.DataFrame) -> pd.DataFrame
         fy_quarters = pd.concat([fy_quarters, pd.DataFrame(q4)], axis='index', ignore_index=True)
     return fy_quarters
 
-'''
-# Gets Q4 value from time series as FY - Q3 - Q2 - Q1
-fiscal_years = []
-for year, fy_data in quarters.groupby('fy'):
-    if (fy_data.fp == 'FY').any() or not (years.fy == year).any(): 
-        fiscal_years.append(fy_data)
-        continue
-    year_row = years[years.fy == year].iloc[0]
-    q1_row, q2_row, q3_row = fy_data[fy_data.fp == 'Q1'].iloc[0], fy_data[fy_data.fp == 'Q2'].iloc[0], fy_data[fy_data.fp == 'Q3'].iloc[0]
-    q4 = {
-        'end': [year_row.end],
-        'val': [year_row.val - q3_row.val - q2_row.val - q1_row.val],
-        'fy': [year],
-        'fp': ['FY'],
-        'form': ['10-K'],
-        'filed': [year_row.filed],
-        'delay': [year_row.delay],
-        'period': [year_row.end - (q3_row.end + timedelta(days=1))],
-    }
-    fy_data = pd.concat([fy_data, pd.DataFrame(q4)], axis='index')
-    fiscal_years.append(fy_data)
+def build_fundamental_col(directory: str, column_name: str, unit: str, column_alias: str, companies) -> pd.DataFrame:
+    time_series = []
+    for company_cik in tqdm(companies):
+        try:
+            company_data = load_company(company_cik, directory)
+            if company_data is None: continue
+            company_data = get_time_series(company_data, column_name, unit)
+            convert_datetimes(company_data)
+            add_time_offsets(company_data)
+            drop_unused_columns(company_data)
+            quarters, years = get_current_quarters_years(company_data)
+            company_data = [add_q4(quarters, years[years.fy == fy]) for fy, fy_quarters in quarters.groupby('fy')]
+            company_data = pd.concat(company_data, ignore_index=True)
+            company_data['cik'] = company_cik
 
-final_df: pd.DataFrame = pd.concat(fiscal_years)
-final_df['fp'] = final_df.fp.map({'Q1': 'Q1', 'Q2': 'Q2', 'Q3': 'Q3', 'FY': 'Q4'})
-final_df.set_index(['fy', 'fp']).sort_index()
-'''
+            company_data.drop(columns=['end', 'fy', 'fp', 'period', 'delay'], inplace=True)
+            company_data.set_index(['cik', 'filed'], inplace=True)
+            company_data.drop_duplicates(inplace=True)
+            time_series.append(company_data)
+        except KeyError as ke:
+            #display(ke)
+            continue
+        except ValueError as ve:
+            #display(ve)
+            continue
+    time_series = pd.concat(time_series).rename({'val': column_alias}, axis='columns')
+    #display(time_series.columns)
+    return time_series
---
+++ stooq.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from random import random
+from pathlib import Path
+
+def load_table(path: Path) -> pd.DataFrame | None:
+    try: 
+        return pd.read_csv(path)
+    except pd.errors.EmptyDataError:
+        return None
+    
+def build_prices_table(directory: str, period: str, market: str) -> pd.DataFrame:
+    pathes = Path(f'{directory}/{period}/{market}').glob(f'* stocks/*/*.{market}.txt')
+    tables = [load_table(path) for path in pathes if path]
+    tables = pd.concat(tables)
+    tables.drop(columns=['<PER>', '<OPENINT>', '<TIME>'], inplace=True, errors='ignore')
+    tables['<DATE>'] = pd.to_datetime(tables['<DATE>'].astype(str))
+    tables.reset_index(drop=True, inplace=True)
+    tables.sort_values(['<TICKER>', '<DATE>'], inplace=True)
+    return tables
+
+def add_one_cent_noise(table: pd.DataFrame) -> pd.DataFrame:
+    table['<CLOSE>'] = table['<CLOSE>'].apply(lambda x: x + (random())*0.01 )
+    return table
+
+def add_columns_prices(table: pd.DataFrame) -> pd.DataFrame:
+    table['<RETURN>'] = table['<CLOSE>'] / table.groupby('<TICKER>')['<CLOSE>'].shift()
+    table['<LOG_CLOSE>'] = np.log(table['<CLOSE>'])
+    table['<LOG_RETURN>'] = np.log(table['<RETURN>'])
+    return table
